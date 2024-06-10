8182ebd5731bb370379e4e8f993aaf7261a3e4dc
Mimenza <mimenzae@gmail.com>
2024-06-10 16:46:25 +0200

    ex00

---
+++ ex00/incs/ClapTrap.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emimenza <emimenza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 15:24:47 by emimenza          #+#    #+#             */
+/*   Updated: 2024/06/10 16:40:20 by emimenza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+# include <string>
+
+class ClapTrap
+{
+	private:
+		std::string _name;
+		int _hitpoints; //health of the player
+		unsigned int _nrgpoints; //energy of the player (stamina)
+		unsigned int _atkpoints; //damage of the player attack
+
+	public:
+		ClapTrap();
+		ClapTrap(std::string name);
+		~ClapTrap();
+
+		ClapTrap(ClapTrap const&);
+		ClapTrap&	operator=(ClapTrap const&);
+
+		void attack(const std::string& target); 
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+		void display(std::ostream& stream) const;
+};
+
+std::ostream&	operator<<(std::ostream& stream, ClapTrap const& cl);
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emimenza <emimenza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 15:23:55 by emimenza          #+#    #+#             */
+/*   Updated: 2024/06/10 16:45:14 by emimenza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "incs/ClapTrap.hpp"
+
+int main(void)
+{
+	ClapTrap	a("Arthur");
+	ClapTrap	b("Dutch");
+
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	b.takeDamage(3);
+	std::cout << b << std::endl;
+	b.takeDamage(20);
+	std::cout << b << std::endl;
+	b.beRepaired(3);
+	std::cout << b << std::endl;
+	b.attack("Eric");
+	b.attack("Eric");
+	std::cout << b << std::endl;
+	b.attack("Eric");
+	std::cout << b << std::endl;
+	b.beRepaired(6);
+	std::cout << b << std::endl;
+	b.attack("Eric");
+}
